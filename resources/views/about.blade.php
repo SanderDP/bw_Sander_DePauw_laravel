f50ceed80d98f5538657ce68fb8b6ee23a685b85
Sander De Pauw <sander.depauw@outlook.be>
2022-12-31 12:06:09 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <ul>
+        <li><a href="https://laravel.com/docs/9.x/installation#your-first-laravel-project">laravel installation</a></li>
+        <li><a href="https://ehb.instructuremedia.com/embed/edeb8a9e-3e8f-4af3-85f9-8be5ae754e7d">teacher video guestbook</a></li>
+        <li><a href="https://techvblogs.com/blog/how-to-install-bootstrap-5-in-laravel-9-with-vite">laravel install bootstrap with vite</a></li>
+        <li><a href="https://laravel.com/docs/4.2/migrations#database-seeding">laravel seeding</a></li>
+    </ul>
+</div>
+@endsection
