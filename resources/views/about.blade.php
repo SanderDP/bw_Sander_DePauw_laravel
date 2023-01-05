6503c0ee862b4456ff7a99267ba7f0cc519972bc
Sander De Pauw <sander.depauw@outlook.be>
2023-01-05 15:43:26 +0100

    Added github link to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <ul>
         <li><a href="https://laravel.com/docs/9.x/installation#your-first-laravel-project">laravel installation</a></li>
+        <li><a href="https://github.com/SanderDP/bw_Sander_DePauw_laravel">My Github location</a></li>
         <li><a href="https://ehb.instructuremedia.com/embed/edeb8a9e-3e8f-4af3-85f9-8be5ae754e7d">teacher video guestbook</a></li>
         <li><a href="https://techvblogs.com/blog/how-to-install-bootstrap-5-in-laravel-9-with-vite">laravel install bootstrap with vite</a></li>
         <li><a href="https://laravel.com/docs/4.2/migrations#database-seeding">laravel seeding</a></li>
