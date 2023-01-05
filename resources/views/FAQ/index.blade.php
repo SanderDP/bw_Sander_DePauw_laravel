d928a8ec76ac231774cb869e7e8f2c1c45b6fdc7
Sander De Pauw <sander.depauw@outlook.be>
2023-01-05 17:39:31 +0100

    added edit, delete and add buttons for categories

--- resources/views/FAQ/index.blade.php
+++ resources/views/FAQ/index.blade.php
@@ -16,7 +16,18 @@
 
                     @foreach($categories as $category)
                     <div class="card mb-3">
-                        <div class="card-header">{{$category->name}}</div>
+                        <div class="card-header"><h2>{{$category->name}}</h2>
+                            @auth
+                                @if (Auth::user()->is_admin)
+                                    <form method="POST" action="{{route('FAQCategories.destroy', $category->id)}}">
+                                        @csrf
+                                        @method("delete")
+                                        <button onclick="location.href='{{route('FAQCategories.edit', $category->id)}}'" type="button" class="btn btn-primary">Edit Category</button>
+                                        <input type="submit" value="Delete Category" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this category?');">
+                                    </form>
+                                @endif
+                            @endauth
+                        </div>
                         <div class="card-body">
                             @foreach ($category->questions as $question)
                                 <div class="card mb-2">
@@ -29,7 +40,11 @@
                         </div>
                     </div>
                     @endforeach
-
+                    @auth
+                        @if (Auth::user()->is_admin)
+                            <button onclick="location.href='{{route('FAQCategories.create')}}'" type="button" class="btn btn-primary">Add New Category</button>
+                        @endif
+                    @endauth
                 </div>
             </div>
         </div>
